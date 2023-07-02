ce566c823948d5fdadba45c781e29a723d005e84
Satyam Sagar <satyamsagar1711@gmail.com>
2023-07-03 03:04:09 +0530

    minor

--- backend/resources/routes.py
+++ backend/resources/routes.py
@@ -1,6 +1,7 @@
-from .students import StudentsApi, CollegeListApi
+from .students import StudentsApi, CollegeListApi, DepartmentListApi
 
 
 def initialize_routes(api):
     api.add_resource(StudentsApi, '/api/students')
     api.add_resource(CollegeListApi, '/api/colleges')
+    api.add_resource(DepartmentListApi, '/api/departments')
